7c866f2bf5c46577d08ba5ce425ea88f844edb92
Allan Esquina <loloudxur@gmail.com>
2013-08-01 12:22:19 +0000

    add

---
+++ app.js
@@ -0,0 +1,33 @@
+var express =  require('express'),
+app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+var userList = [];
+
+server.listen(8080);
+
+
+app.use(express.static(__dirname + '/static'));
+app.use('/ja', express.static(__dirname + '/static/js'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) { 
+  
+	socket.on('sendMsg', function(data) {
+		io.sockets.emit('wMsg', {
+			content: socket.id + " : " + data
+		});
+		//socket.emit('wMsg', { content: data });
+	});
+
+	socket.on('userProfile', function(data) {
+		data.id = socket.id;
+		userList.push(data);
+		console.log(userList);
+	});
+
+});
