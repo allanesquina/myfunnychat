6091c284cf5767a45e5891d9d39832b31b6f24b7
Allan Esquina <loloudxur@gmail.com>
2013-08-01 13:41:19 +0000

    ajustes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express =  require('express'),
-app = express(),
+var express = require('express'),
+	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 
@@ -7,26 +7,23 @@ var userList = [];
 
 server.listen(8080);
 
-
 app.use(express.static(__dirname + '/static'));
-app.use('/ja', express.static(__dirname + '/static/js'));
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) { 
-  
+io.sockets.on('connection', function(socket) {
+
 	socket.on('sendMsg', function(data) {
 		io.sockets.emit('wMsg', {
-			content: socket.id + " : " + data
+			content: userList[socket.id].name + " : " + data
 		});
 		//socket.emit('wMsg', { content: data });
 	});
 
 	socket.on('userProfile', function(data) {
-		data.id = socket.id;
-		userList.push(data);
+		userList[socket.id] = data;
 		console.log(userList);
 	});
 
